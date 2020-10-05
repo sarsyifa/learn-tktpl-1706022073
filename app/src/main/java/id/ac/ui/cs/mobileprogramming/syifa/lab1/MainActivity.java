bd1511bcc4a8a2f3268bfc22f39bfce6bef6667a
Siti Aulia Rahmatussyifa <sarsyifa@gmail.com>
2020-10-05 14:55:59 +0700

    add activity basic stopwatch

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/syifa/lab1/MainActivity.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/syifa/lab1/MainActivity.java
@@ -2,55 +2,146 @@ package id.ac.ui.cs.mobileprogramming.syifa.lab1;
 
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 
-import android.view.Menu;
-import android.view.MenuItem;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Handler;
+import android.util.Log;
+import android.widget.TextView;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int seconds = 0;
+    private boolean running = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Log.d("onCreate, seconds", Integer.toString(seconds));
+        Log.d("onCreate, running", Boolean.toString(running));
+
+        activateTimer();
+    }
+
+    /**
+     * Sets the Number of seconds on the timer.
+     * The activateTimer() method uses a Handler to increment the seconds and update the text view.
+     *
+     */
+    private void activateTimer() {
+        final TextView timeView = (TextView)findViewById(R.id.time_view);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Under Construction", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void run() {
+                int hours = seconds/3600;
+                int minutes = (seconds%3600)/60;
+                int secs = seconds%60;
+
+                String time = String.format(Locale.getDefault(),
+                        "%02d.%02d.%02d", hours, minutes, secs);
+                timeView.setText(time);
+                if (running) {
+                    seconds++;
+                }
+                handler.postDelayed(this, 1000);
             }
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    /**
+     * Start the stopwatch running when the Start button is clicked.
+     * Below method gets called when the Start button is clicked.
+     *
+     */
+    public void onStartButtonClicked(View view) {
+        Log.d("onStart, seconds", Integer.toString(seconds));
+        Log.d("onStart, running", Boolean.toString(running));
+
+        running = true;
+    }
+
+    /**
+     * Stop the stopwatch running when the Stop button is clicked.
+     * Below method gets called when the Stop button is clicked.
+     *
+     */
+    public void onStopButtonClicked(View view) {
+        Log.d("onStop, seconds", Integer.toString(seconds));
+        Log.d("onStop, running", Boolean.toString(running));
+
+        running = false;
+    }
+
+    /**
+     * Reset the stopwatch running when the Reset button is clicked.
+     * Below method gets called when the Reset button is clicked.
+     *
+     */
+    public void onResetButtonClicked(View view) {
+        Log.d("onReset, seconds", Integer.toString(seconds));
+        Log.d("onReset, running", Boolean.toString(running));
+
+        running = false;
+        seconds = 0;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    protected void onStart() {
+        super.onStart();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
+
+    /**
+     * If the activity is resumed,
+     *  start the stopwatch again if it was running previously.
+     *
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("onResume, seconds", Integer.toString(seconds));
+        Log.d("onResume, running", Boolean.toString(running));
+    }
+
+    /**
+     * If the activity is paused, stop the stopwatch.
+     *
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("onPause, seconds", Integer.toString(seconds));
+        Log.d("onPause, running", Boolean.toString(running));
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("onRestart, seconds", Integer.toString(seconds));
+        Log.d("onRestart, running", Boolean.toString(running));
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("onDestroy, seconds", Integer.toString(seconds));
+        Log.d("onDestroy, running", Boolean.toString(running));
+    }
+
 }
