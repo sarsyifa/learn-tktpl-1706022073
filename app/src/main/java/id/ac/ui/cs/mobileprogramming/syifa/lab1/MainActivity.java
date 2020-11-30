b88747cbcca10406111537ee9be3c4d8b3870466
Siti Aulia Rahmatussyifa <sarsyifa@gmail.com>
2020-11-30 19:24:19 +0700

    implement get list of wifi access points

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/syifa/lab1/MainActivity.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/syifa/lab1/MainActivity.java
@@ -1,19 +1,37 @@
 package id.ac.ui.cs.mobileprogramming.syifa.lab1;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.Uri;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import com.android.volley.Cache;
 
+import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WifiManager wifiManager;
+    private final ArrayList<String> wifiList = new ArrayList<>();
+    private ArrayAdapter<String> aAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +40,25 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button scanButton = findViewById(R.id.scan_button);
+        scanButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Under Construction", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                scanWifi();
             }
         });
+
+        ListView listView = findViewById(R.id.list_wifi);
+        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+
+        if (!wifiManager.isWifiEnabled()) {
+            wifiManager.setWifiEnabled(true);
+        }
+
+        aAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, wifiList);
+        listView.setAdapter(aAdapter);
+        scanWifi();
+
     }
 
     @Override
@@ -48,9 +77,39 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                    Uri.fromParts("package", getPackageName(), null));
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
+
+    private void scanWifi() {
+        wifiList.clear();
+        registerReceiver(wifiReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+        wifiManager.startScan();
+        Toast.makeText(this, "Scanning WiFi ...", Toast.LENGTH_SHORT).show();
+    }
+
+    BroadcastReceiver wifiReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            List<ScanResult> results = wifiManager.getScanResults();
+            unregisterReceiver(this);
+
+            for (ScanResult scanResult : results) {
+                if (!scanResult.SSID.isEmpty()) {
+                    wifiList.add(scanResult.SSID);
+                    Log.d("wifiList", scanResult.SSID);
+                }
+            }
+
+            aAdapter.notifyDataSetChanged();
+        }
+    };
+
+
 }
