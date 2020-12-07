d9bcb4d284f00165f65e49f8f11781369b6e390c
Siti Aulia Rahmatussyifa <sarsyifa@gmail.com>
2020-12-07 19:54:59 +0700

    implement jni

--- app/src/main/cpp/string-jni.cpp
+++ app/src/main/cpp/string-jni.cpp
@@ -2,3 +2,21 @@
 // Created by D E L L on 07/12/2020.
 //
 
+#include <string.h>
+#include <jni.h>
+
+extern "C"
+JNIEXPORT jstring
+
+JNICALL
+Java_id_ac_ui_cs_mobileprogramming_syifa_lab1_MainActivity_helloFromJNI(JNIEnv *env, jobject thiz, jstring input_name) {
+    // TODO: implement helloFromJNI()
+    const char *name = env -> GetStringUTFChars(input_name, NULL);
+    char message[60] = "Hello ";
+
+    strcat(message, name);
+
+    env -> ReleaseStringUTFChars(input_name, name);
+    return env->NewStringUTF(message);
+}
+
--- app/src/main/java/id/ac/ui/cs/mobileprogramming/syifa/lab1/MainActivity.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/syifa/lab1/MainActivity.java
@@ -2,9 +2,6 @@ package id.ac.ui.cs.mobileprogramming.syifa.lab1;
 
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -12,8 +9,21 @@ import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    static {
+        System.loadLibrary("string-jni");
+    }
+
+    private ImageView iViewDice;
+    private Random rnd = new Random();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,15 +32,28 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        iViewDice = findViewById(R.id.image_view_dice);
+        iViewDice.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Under Construction", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                rollDice();
+            }
+        });
+
+        Button helloButton = findViewById(R.id.button);
+        final EditText inputName = findViewById(R.id.inputName);
+
+        helloButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String greeting = helloFromJNI(inputName.getText().toString());
+
+                Toast.makeText(getApplicationContext(), greeting, Toast.LENGTH_LONG).show();
             }
         });
     }
+
+    public native String helloFromJNI(String inputName);
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -53,4 +76,29 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private void rollDice() {
+        int randomNumber = rnd.nextInt(6) + 1;
+        switch (randomNumber) {
+            case 1:
+                iViewDice.setImageResource(R.drawable.dice1);
+                break;
+            case 2:
+                iViewDice.setImageResource(R.drawable.dice2);
+                break;
+            case 3:
+                iViewDice.setImageResource(R.drawable.dice3);
+                break;
+            case 4:
+                iViewDice.setImageResource(R.drawable.dice4);
+                break;
+            case 5:
+                iViewDice.setImageResource(R.drawable.dice5);
+                break;
+            case 6:
+                iViewDice.setImageResource(R.drawable.dice6);
+                break;
+        }
+    }
+
 }
